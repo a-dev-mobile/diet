eb6dbb019aa608ee471be8ddb5a1c28213dd1b5d
trofimov <dev.mobile@yandex.ru>
2020-11-08 01:14:10 +0300

    add bottom nav

--- app/src/main/java/a/dev/mobile/diet/MainActivity.kt
+++ app/src/main/java/a/dev/mobile/diet/MainActivity.kt
@@ -2,10 +2,26 @@ package a.dev.mobile.diet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+
+        val appBarConfig =
+            AppBarConfiguration(setOf(R.id.firstFragment, R.id.secondFragment, R.id.thirdFragment))
+        setupActionBarWithNavController(navController, appBarConfig)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+
     }
 }
