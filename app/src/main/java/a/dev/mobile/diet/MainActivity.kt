8021779c8989e5de753ffe89bf18345ef9a36855
trofimov <dev>
2020-11-11 16:14:17 +0300

    del test file add example firebase

--- app/src/main/java/a/dev/mobile/diet/MainActivity.kt
+++ app/src/main/java/a/dev/mobile/diet/MainActivity.kt
@@ -8,7 +8,10 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
+import kotlin.collections.HashMap
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfig)
 
         bottomNavigationView.setupWithNavController(navController)
+
 
         val db = FirebaseFirestore.getInstance()
 
@@ -62,6 +66,154 @@ class MainActivity : AppCompatActivity() {
 
 
         addToDB(db, user)
+
+
+        //==============================
+        val cities2 = db.collection("cities2")
+
+        val data1 = hashMapOf(
+            "name" to "San Francisco",
+            "state" to "CA",
+            "country" to "USA",
+            "capital" to false,
+            "population" to 860000,
+            "regions" to listOf("west_coast", "norcal")
+        )
+        cities2.document("SF").set(data1)
+
+        val data2 = hashMapOf(
+            "name" to "Los Angeles",
+            "state" to "CA",
+            "country" to "USA",
+            "capital" to false,
+            "population" to 3900000,
+            "regions" to listOf("west_coast", "socal")
+        )
+        cities2.document("LA").set(data2)
+
+        val data3 = hashMapOf(
+            "name" to "Washington D.C.",
+            "state" to null,
+            "country" to "USA",
+            "capital" to true,
+            "population" to 680000,
+            "regions" to listOf("east_coast")
+        )
+        cities2.document("DC").set(data3)
+
+        val data4 = hashMapOf(
+            "name" to "Tokyo",
+            "state" to null,
+            "country" to "Japan",
+            "capital" to true,
+            "population" to 9000000,
+            "regions" to listOf("kanto", "honshu")
+        )
+        cities2.document("TOK").set(data4)
+
+        val data5 = hashMapOf(
+            "name" to "Beijing",
+            "state" to null,
+            "country" to "China",
+            "capital" to true,
+            "population" to 21500000,
+            "regions" to listOf("jingjinji", "hebei")
+        )
+        cities2.document("BJ").set(data5)
+
+
+//==============================
+        val docData = hashMapOf(
+            "stringExample" to "Hello world!",
+            "booleanExample" to true,
+            "numberExample" to 3.14159265,
+            "dateExample" to Timestamp(Date()),
+            "listExample" to arrayListOf(1, 2, 3),
+            "nullExample" to null
+        )
+
+        val nestedData = hashMapOf(
+            "a" to 5,
+            "b" to true
+        )
+
+        docData["objectExample"] = nestedData
+
+        db.collection("data").document("one")
+            .set(docData)
+            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+            .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+
+
+
+
+
+
+
+
+        val citiesRef = db.collection("cities")
+
+        val ggbData = mapOf(
+            "name" to "Golden Gate Bridge",
+            "type" to "bridge"
+        )
+        citiesRef.document("SF").collection("landmarks").add(ggbData)
+
+        val lohData = mapOf(
+            "name" to "Legion of Honor",
+            "type" to "museum"
+        )
+        citiesRef.document("SF").collection("landmarks").add(lohData)
+
+        val gpData = mapOf(
+            "name" to "Griffth Park",
+            "type" to "park"
+        )
+        citiesRef.document("LA").collection("landmarks").add(gpData)
+
+        val tgData = mapOf(
+            "name" to "The Getty",
+            "type" to "museum"
+        )
+        citiesRef.document("LA").collection("landmarks").add(tgData)
+
+        val lmData = mapOf(
+            "name" to "Lincoln Memorial",
+            "type" to "memorial"
+        )
+        citiesRef.document("DC").collection("landmarks").add(lmData)
+
+        val nasaData = mapOf(
+            "name" to "National Air and Space Museum",
+            "type" to "museum"
+        )
+        citiesRef.document("DC").collection("landmarks").add(nasaData)
+
+        val upData = mapOf(
+            "name" to "Ueno Park",
+            "type" to "park"
+        )
+        citiesRef.document("TOK").collection("landmarks").add(upData)
+
+        val nmData = mapOf(
+            "name" to "National Musuem of Nature and Science",
+            "type" to "museum"
+        )
+        citiesRef.document("TOK").collection("landmarks").add(nmData)
+
+        val jpData = mapOf(
+            "name" to "Jingshan Park",
+            "type" to "park"
+        )
+        citiesRef.document("BJ").collection("landmarks").add(jpData)
+
+        val baoData = mapOf(
+            "name" to "Beijing Ancient Observatory",
+            "type" to "musuem"
+        )
+        citiesRef.document("BJ").collection("landmarks").add(baoData)
+
+
 
 
     }
